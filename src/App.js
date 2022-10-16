3400e54c39c97dd8ac176cc0dcf0f2ef41ff0e13
Nishi14jain <nishi.jain14112001@gmail.com>
2022-10-16 21:13:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> heyyy</h1>
+    <p> I Love mY MOM</p>
+    
+    </div>
+  );
+}
+
+export default App;
+  
